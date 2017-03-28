e38b20fab5409fb51d8bddcfdfdfeaba716b17a8
Tobias Davis <tdavis@fnoex.com>
2017-03-28 15:01:08 -0500

    add pseudo test that checks self references

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const Demo = require('./Demo.html')
 const ThrowingComponent = require('./ThrowingComponent.html')
 const FileWithDashes = require('./file-with-dashes.html')
+const ReferenceSelf = require('./ReferenceSelf.html')
 
 const demo = new Demo({
 	target: document.querySelector('#demo')
@@ -12,6 +13,18 @@ demo.set({
 
 new FileWithDashes({
 	target: document.querySelector('#fileWithDashesComponent')
+})
+
+new ReferenceSelf({
+	target: document.querySelector('#referenceSelf'),
+	data: {
+		items: [{
+			label: 'parent',
+			children: [{
+				label: 'child'
+			}]
+		}]
+	}
 })
 
 new ThrowingComponent({
