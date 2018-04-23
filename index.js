b9ccf6da6a3a2cb63e191a2e76f4e6cd8d989107
Josh Duff <me@JoshDuff.com>
2018-04-23 10:50:19 -0500

    Svelte 2 compatibility
    Fixes #8

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { basename, extname } = require('path')
-const { compile } = require('svelte')
-const through = require('through2')
+const { basename, extname } = require(`path`)
+const { compile } = require(`svelte`)
+const through = require(`through2`)
+const toPascalCase = require(`just-pascal-case`)
 
-const defaultExtensions = [ '.html', '.svelte' ]
+const defaultExtensions = [ `.html`, `.svelte` ]
 
 module.exports = function transformSvelte(file, options) {
-	let data = ''
+	let data = ``
 
 	const extensionsArray = (options.extensions && options.extensions._) || defaultExtensions
 
@@ -17,17 +18,18 @@ module.exports = function transformSvelte(file, options) {
 			cb()
 		}, function end(cb) {
 			const base = basename(file)
-			const name = sanitizeJavaScriptFunctionName(base.replace(extension, ''))
+			const name = toPascalCase(base.replace(extension, ``))
 
 			try {
 				const svelteOptions = Object.assign({}, options.svelte, {
 					name,
 					filename: base,
-					format: 'cjs'
+					format: `cjs`,
 				})
-				const { code, map } = compile(data, svelteOptions)
+				const { js } = compile(data, svelteOptions)
+				const { code, map } = js
 				this.push(code)
-				this.push('\n//# sourceMappingURL=' + map.toUrl())
+				this.push(`\n//# sourceMappingURL=` + map.toUrl())
 
 				cb()
 			} catch (err) {
@@ -41,8 +43,4 @@ module.exports = function transformSvelte(file, options) {
 
 function extensionIsInList(extension, extensionsArray) {
 	return extensionsArray.indexOf(extension.toLowerCase()) !== -1
-}
-
-function sanitizeJavaScriptFunctionName(text) {
-	return text.replace(/\W/g, '')
 }
